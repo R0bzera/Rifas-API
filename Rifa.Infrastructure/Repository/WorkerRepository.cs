9a7df799b629c38c36a01deb20b98c106f1cde1a
Lucas Fraporti <lucasfraporti1999@gmail.com>
2025-11-22 15:05:55 -0300

    Ajustando fluxo

--- Rifa.Infrastructure/Repository/WorkerRepository.cs
+++ Rifa.Infrastructure/Repository/WorkerRepository.cs
@@ -35,12 +35,21 @@ namespace Rifa.Infrastructure.Repository
 
             foreach (var item in rifasParaFinalizar)
             {
-                var paymentOrder = await _context.payment_orders.FirstOrDefaultAsync(p => p.PedidoId == item);
                 var cotasRifa = await _context.cotas_rifa.FirstOrDefaultAsync(p => p.PedidoId == item);
-                paymentOrder.Status = "expired";
-                cotasRifa.PedidoId = null;
-                cotasRifa.UsuarioId = null;
-                await _context.SaveChangesAsync();
+                if (cotasRifa != null)
+                {
+                    cotasRifa.PedidoId = null;
+                    cotasRifa.UsuarioId = null;
+
+                    await _context.SaveChangesAsync();
+                }
+
+                var pedidosRifas = await _context.pedidos_rifa.FirstOrDefaultAsync(p => p.Id == item);
+                if (pedidosRifas != null)
+                {
+                    _context.pedidos_rifa.Remove(pedidosRifas);
+                    await _context.SaveChangesAsync();
+                }
             }
 
         }
